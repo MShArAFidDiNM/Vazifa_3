c536e15ec8055336fe7edb10ee7feedc3e5d27a1
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-03-28 13:26:17 +0500

    Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number : ");
+            string str = Console.ReadLine();
+            bool check = int.TryParse(str, out int number);
+            int factorial = 1;
+            for (int i = number; i > 0; i--)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine($"The factorial value of the number {number} is equal to {factorial} ");
+        }
+    }
+}
