0e1009cbb4397f01ffc0dd7f5e2c4df66d3ae234
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-03-28 13:32:06 +0500

    Create Program.cs

---
+++ Celsiy/Vazifa_Tempratura/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Vazifa_Tempratura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = 3;
+            qaytakirit:
+            Console.Write("Enter Celcius : ");
+            f--;
+            string c = Console.ReadLine();
+            bool t = double.TryParse(c, out double r);
+                if (!t)
+                {
+                    Console.WriteLine($"Error !!! {c}");                    
+                    if (f <= 0)
+                    {
+                        Console.WriteLine("Your attempt has ended. Please try again later");
+                    return;
+                    }
+                    Console.WriteLine("Enter again");
+                    Console.WriteLine($"You have {f} attempts left");                 
+                    goto qaytakirit;   
+                } 
+                else
+            {
+                Console.WriteLine($"Kelvin : {r * 10.1}");
+                Console.WriteLine($"Farengeyt : {r * 2.86666667}");
+            }
+            Main(args);
+        }
+    }
+}
