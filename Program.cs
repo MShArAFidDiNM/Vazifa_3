551ed9429e6b76aa130934f2462bf0e5702d076f
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-03-28 13:23:50 +0500

    tub son

--- Program.cs
+++ Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _77777
+﻿namespace Tub_son
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter Number : ");
+            string a = Console.ReadLine();
+            bool b = int.TryParse(a, out int c);
+            int g = 0;
+            if (b) {
+                for (int i = 2; i <= c; i++)
+                {
+                    if (c % i == 0)
+                    {
+                        g++;
+                    }                   
+                }
+                if (g == 1)
+                {
+                    Console.WriteLine($"{a} a prime number");
+                }
+                else
+                {
+                    Console.WriteLine($"{a} is not a prime number");
+
+                }
+            }else
+            {
+                Console.WriteLine("You didn't enter a number");
+            }
+            Main(args);
         }
     }
 }
